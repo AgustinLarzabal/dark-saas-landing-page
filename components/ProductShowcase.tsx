4768e232842a543dbcddb96af6759b782a48a7a8
Agustin Larzabal <agustin.larzabal@globant.com>
2024-08-19 22:27:02 +0200

    add animation to the product showcase section

--- components/ProductShowcase.tsx
+++ components/ProductShowcase.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
-import appScreen from "../assets/images/app-screen.png";
+import { useRef } from "react";
+
+import appScreen from "@/assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
       <div className="container">
@@ -15,7 +28,20 @@ export const ProductShowcase = () => {
             one task at a time.
           </p>
         </div>
-        <Image src={appScreen} alt="The product screenshot" className="mt-14" />
+        <motion.div
+          style={{
+            opacity,
+            rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            ref={appImage}
+            src={appScreen}
+            alt="The product screenshot"
+            className="mt-14"
+          />
+        </motion.div>
       </div>
     </div>
   );
