0a8e39e6d7960f5d8ad454dcb57b588a29b1693d
Agustin Larzabal <agustin.larzabal@globant.com>
2024-08-19 21:56:25 +0200

    add animetion to the logo ticket section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from "next/image";
-
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 import ArrowWIcon from "@/assets/icons/arrow-w.svg";
 import cursorImage from "@/assets/images/cursor.png";
--- components/LogoTicket.tsx
+++ components/LogoTicket.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
-import acmeLogo from "../assets/images/acme.png";
-import apexLogo from "../assets/images/apex.png";
-import celectialLogo from "../assets/images/celestial.png";
-import echoLogo from "../assets/images/echo.png";
-import pulseLogo from "../assets/images/pulse.png";
-import quantumLogo from "../assets/images/quantum.png";
+
+import acmeLogo from "@/assets/images/acme.png";
+import apexLogo from "@/assets/images/apex.png";
+import celectialLogo from "@/assets/images/celestial.png";
+import echoLogo from "@/assets/images/echo.png";
+import pulseLogo from "@/assets/images/pulse.png";
+import quantumLogo from "@/assets/images/quantum.png";
 
 const images = [
   { src: acmeLogo, alt: "Acme logo" },
@@ -22,8 +26,17 @@ export const LogoTicker = () => {
         <h2 className="text-xl text-center text-white/70">
           Trusted by the world&apos;s most innovative teams
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-16">
+        <div className="flex overflow-hidden mt-9 before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <motion.div
+            transition={{
+              duration: 20,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            className="flex gap-16 flex-none pr-16"
+          >
             {images.map(({ src, alt }) => (
               <Image
                 key={alt}
@@ -32,7 +45,15 @@ export const LogoTicker = () => {
                 className="flex-none h-8 w-auto"
               />
             ))}
-          </div>
+            {images.map(({ src, alt }) => (
+              <Image
+                key={alt}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto "
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
